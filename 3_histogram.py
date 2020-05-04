726da872a1dc7e42b560045b57bf264d1eabf2ca
Ceciliadc <64777292+Ceciliadc@users.noreply.github.com>
2020-05-04 15:05:25 +0200

    Update and rename histogram.py to 3_histogram.py

--- 3_histogram.py
+++ 3_histogram.py
@@ -1,9 +1,9 @@
-'''Your code will take as input a grayscale image im (a np.ndarray with dtype np.uint8 and shape (3, H, W)) and an integer nbin. It should compute a normalized color histogram of the image, quantized with nbin bins on each color plane.
+'''Your code will take as input a grayscale image im (a np.ndarray with dtype np.uint8 and shape (3, H, W)) and an integer nbin. 
+It should compute a normalized color histogram of the image, quantized with nbin bins on each color plane.
 
-The output should be a np.ndarray with shape (3*nbin, ), containing the concatenation of the histograms computed on each color plane (in the same order of the input tensor).
-
+The output should be a np.ndarray with shape (3*nbin, ), containing the concatenation of the histograms computed on each color plane 
+(in the same order of the input tensor).
 The output should be L1-normalized (i.e. all bins of the final histogram should sum up to 1).
-
 Quantization strategy: a pixel should go in the bin with index b iif: pixel*n_bin // 256 == b'''
 
 import random
