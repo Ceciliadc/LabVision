19f6c8de887ea669a2c89d07c52849ac679bfba7
Ceciliadc <64777292+Ceciliadc@users.noreply.github.com>
2020-05-04 15:07:46 +0200

    Update and rename thresholding.py to 7_thresholding.py

--- 7_thresholding.py
+++ 7_thresholding.py
@@ -1,7 +1,7 @@
-'''Given an input grayscale image im (a np.ndarray with shape (H, W) and dtype np.uint8), write a code which performs a binary thresholding of the image at cut value val, and stores the result in out.
+'''Given an input grayscale image im (a np.ndarray with shape (H, W) and dtype np.uint8), write a code which performs a binary thresholding of 
+the image at cut value val, and stores the result in out.
 
 out should be another image, with the same shape of im, and with all the pixels greater than the threshold set to 255, all the others set to 0.
-
 Be careful not to modify the original tensor in-place: the function should perform the thresholding on a copy of the image.'''
 
 import random
